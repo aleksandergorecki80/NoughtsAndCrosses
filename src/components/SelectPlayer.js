99d805ff8262f58332a508971b3c368f4437c46d
FoxCote <aleksandergorecki80@gmail.com>
2020-12-23 22:12:03 +0000

    Add a svg file for nought

--- src/components/SelectPlayer.js
+++ src/components/SelectPlayer.js
@@ -36,7 +36,8 @@ const containerVariants = {
 
 const SelectPlayer = (props) => {
   const onSelectPlayer = (event) => {
-    props.dispatch(selectPlayer(event.target.value));
+    console.log(event.target.attributes.value.value)
+    props.dispatch(selectPlayer(event.target.attributes.value.value));
     props.dispatch(turnGameOn());
   }
 
@@ -48,8 +49,23 @@ const SelectPlayer = (props) => {
       exit="exit"
     >
 
-      <Link to="/game">
-        <button value="O" onClick={onSelectPlayer}>O</button>
+      <Link to="/game" className="select-player__button" onClick={onSelectPlayer} value="kki">
+        <svg 
+        width="75" 
+        height="75" 
+        xmlns="http://www.w3.org/2000/svg"
+        
+        >
+        <g>
+          <title>O</title>
+          <path 
+          id="svg_1" 
+          className="nought-path"
+          d="m1.875,37.75c0,-19.75138 15.99862,-35.75 35.75,-35.75c19.75138,0 35.75,15.99862 35.75,35.75c0,19.75138 -15.99862,35.75 -35.75,35.75c-19.75138,0 -35.75,-15.99862 -35.75,-35.75z" strokeWidth="1.5" stroke="#000" fill="none"/>
+        </g>
+</svg>
+
+
       </Link>
       <Link to="/game">
         <button value="X" onClick={onSelectPlayer}>X</button>
